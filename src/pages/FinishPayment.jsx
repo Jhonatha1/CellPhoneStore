7dce8a0148af7696dc546bee4593093f3d506f0b
Yanky Jhonatha <yankyboa@gmail.com>
2023-09-21 00:48:12 -0300

    atts

--- src/pages/FinishPayment.jsx
+++ src/pages/FinishPayment.jsx
@@ -1,48 +1,59 @@
-import React, { useState } from 'react'
-import {getItem} from '../services/LocalStorageFuncs'
-import {AiFillCheckCircle} from 'react-icons/ai'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import { getItem } from '../services/LocalStorageFuncs';
+import { AiFillCheckCircle } from 'react-icons/ai';
+import styled from 'styled-components';
 import { Loading } from '../components/Loading';
-import {MdCancel} from 'react-icons/md'
+import { MdCancel } from 'react-icons/md';
 
-const PaymentArea= styled.div`
-    span{
-        font-size: 90px;
-        color: green;
-        text-align: center;
-    }
+const PaymentArea = styled.div`
+  span {
+    font-size: 90px;
+    color: green;
+    text-align: center;
+  }
 `;
+
 export const FinishPayment = (props) => {
-    const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
-    setTimeout(() => {
-        setLoading(false)
-    }, 2500);
-
-
-    const{params:{price}} = props.match
-    const user = getItem('user')
-  return (
+  useEffect(() => {
+    // Carregar o usuário do armazenamento local aqui
+    const loadedUser = getItem('user');
+    setUser(loadedUser);
     
-    <>{
-        loading ? <Loading/> :(
-            
-        user && user.saldo < price ?(
-            <div>
-                <span><MdCancel style={{fontSize:'45px', color:'red'}}/></span><p>Seu saldo é insuficiente!</p>
-                </div> ) :(
-    <PaymentArea>
-    <h3>Sua compra foi concluída com sucesso!!</h3>
-    <span><AiFillCheckCircle /></span>
-    <h4>{`Valor: R$${price}`}</h4>
-    <h4>{`Comprador: ${user.name}`}</h4>
-    <h4>{`Prazo: ${Math.ceil(Math.random())*20+1}`} dias</h4>
-    <h4>{`Seu saldo da carteira agora é: R$${user.saldo - price}`}</h4>
-    </PaymentArea>
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
 
+  const { params: { price } } = props.match;
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        user && user.saldo !== undefined ? (
+          user.saldo < price ? (
+            <div>
+              <span><MdCancel style={{ fontSize: '45px', color: 'red' }} /></span>
+              <p>Seu saldo é insuficiente!</p>
+            </div>
+          ) : (
+            <PaymentArea>
+              <h3>Sua compra foi concluída com sucesso!!</h3>
+              <span><AiFillCheckCircle /></span>
+              <h4>{`Valor: R$${price}`}</h4>
+              <h4>{`Comprador: ${user.name}`}</h4>
+              <h4>{`Prazo: ${Math.ceil(Math.random()) * 20 + 1}`} dias</h4>
+              <h4>{`Seu saldo da carteira agora é: R$${user.saldo - price}`}</h4>
+            </PaymentArea>
+          )
+        ) : (
+          <p>Usuário não encontrado ou saldo não definido.</p>
         )
-    )
-    }
+      )}
     </>
-  )
-}
+  );
+};
