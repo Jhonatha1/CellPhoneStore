e47e16c8ff833a40722be352f40ca44b9cc91653
Yanky Jhonatha <yankyboa@gmail.com>
2023-09-21 01:53:02 -0300

    atts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import './App.css';
 import { Content } from './Content';
 import { Header } from './components/Header';
-import { Login } from './Login';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/inicial" component={Login} />
-        <Redirect from="/" to="/inicial" />
-      </Switch>
+      
+      <Header/>
+      <Redirect from="/" to="/inicial" />
       <Content />
     </div>
   );
